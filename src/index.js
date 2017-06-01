0a6080082821b0de43883d88304cc76cceab97b5
Sean McCullough <smm@architech.ca>
2017-06-01 16:00:07 -0400

    Section 1 - Lesson 27

--- src/index.js
+++ src/index.js
@@ -22,6 +22,7 @@ class App extends Component {
         return (
             <div>
                 <SearchBar />
+                
                 <VideoList videos={this.state.videos} />
             </div>
         );
