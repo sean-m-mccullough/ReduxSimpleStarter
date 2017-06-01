b1e314f528f285c970fa9f3293ca1d8036f197e7
Sean McCullough <smm@architech.ca>
2017-06-01 11:39:43 -0400

    Section 1 - Lesson 14

--- src/index.js
+++ src/index.js
@@ -1,10 +1,16 @@
 import React from 'react';
 import ReactDom from 'react-dom';
 
+import SearchBar from './components/search_bar';
+
 const API_KEY = 'AIzaSyCk5MWRRq20QqCm3wfh9Ti_9I4lD2cTlzw';
 
 const App = () => {
-    return <div>Hi!</div>;
+    return (
+        <div>
+            <SearchBar />
+        </div>
+    );
 }
 
 ReactDom.render(<App />, document.querySelector('.container'));
