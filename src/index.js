c370019646bb4221af365656750ee9b0c1bbe4be
Sean McCullough <smm@architech.ca>
2017-06-01 15:16:18 -0400

    Section 1 - Lesson 22

--- src/index.js
+++ src/index.js
@@ -1,20 +1,29 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDom from 'react-dom';
 import YTSearch from 'youtube-api-search';
 
 import SearchBar from './components/search_bar';
 import YT_API from './keys/youtube-key';
 
-YTSearch({key: YT_API.key, term: 'surfboards'}, function(data) {
-    console.log(data);
-});
 
-const App = () => {
-    return (
-        <div>
-            <SearchBar />
-        </div>
-    );
+class App extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = { videos: [] };
+
+        YTSearch({key: YT_API.key, term: 'surfboards'}, videos => {
+            this.setState({ videos });
+        });
+    }
+
+    render() {
+        return (
+            <div>
+                <SearchBar />
+            </div>
+        );
+    }
 }
 
 ReactDom.render(<App />, document.querySelector('.container'));
