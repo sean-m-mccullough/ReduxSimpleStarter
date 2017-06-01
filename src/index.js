c523968c271e1037bc8545ffd12e4de695d2b57e
Sean McCullough <smm@architech.ca>
2017-06-01 14:46:52 -0400

    Section 1 - Lesson 21

--- src/index.js
+++ src/index.js
@@ -1,9 +1,14 @@
 import React from 'react';
 import ReactDom from 'react-dom';
+import YTSearch from 'youtube-api-search';
 
 import SearchBar from './components/search_bar';
 
 const API_KEY = 'AIzaSyCk5MWRRq20QqCm3wfh9Ti_9I4lD2cTlzw';
+
+YTSearch({key: API_KEY, term: 'surfboards'}, function(data) {
+    console.log(data);
+});
 
 const App = () => {
     return (
