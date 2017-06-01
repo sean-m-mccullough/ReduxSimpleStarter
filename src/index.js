5886f6911d56b6c2d7a3e979d79155601d05f73f
Sean McCullough <smm@architech.ca>
2017-06-01 16:05:28 -0400

    Section 1 - Lesson 28

--- src/index.js
+++ src/index.js
@@ -3,7 +3,9 @@ import ReactDom from 'react-dom';
 import YTSearch from 'youtube-api-search';
 
 import SearchBar from './components/search_bar';
+import VideoDetail from './components/video_detail';
 import VideoList from './components/video_list';
+
 import YT_API from './keys/youtube-key';
 
 
@@ -22,7 +24,7 @@ class App extends Component {
         return (
             <div>
                 <SearchBar />
-                
+                <VideoDetail video={this.state.videos[0]} />
                 <VideoList videos={this.state.videos} />
             </div>
         );
