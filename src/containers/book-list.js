936e17a8cf89247b07356261a4f799fc4dc372d9
Sean McCullough <smm@architech.ca>
2017-06-02 14:15:53 -0400

    Section 4 - Lessonn 42

--- src/containers/book-list.js
+++ src/containers/book-list.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 
-export default class BookList extends Component {
+class BookList extends Component {
     renderList() {
         return this.props.books.map(book => {
             return (
-                <li key={book.title}>{book.title}</li>
+                <li key={book.title} className="list-group-item">{book.title}</li>
             );
         });
     }
@@ -17,3 +18,11 @@ export default class BookList extends Component {
         );
     }
 }
+
+function mapStateToProps(state) {
+    return {
+        books: state.books
+    };
+}
+
+export default connect(mapStateToProps)(BookList);
