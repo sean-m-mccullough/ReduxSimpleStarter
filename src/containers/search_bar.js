7be919fb979b93c8dd51f9b520e6197c996bb725
Sean McCullough <smm@architech.ca>
2017-07-13 16:29:39 -0400

    added form and user inputs

---
+++ src/containers/search_bar.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+export default class SearchBar extends Component {
+    render() {
+        return (
+            <form className="input-group">
+                <input />
+                <span className="input-group-button">
+                    <button type="submit" className="btn btn-secondary">Submit</button>
+                </span>
+            </form>
+        );
+    }
+}
