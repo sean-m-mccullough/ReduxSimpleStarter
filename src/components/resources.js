9e6ac369ff0ba3734387f4b11e3cdc838b892ece
Sean McCullough <smm@architech.ca>
2017-07-21 14:37:13 -0400

    STUFF

---
+++ src/components/resources.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+export default () => {
+    return (
+        <div>
+            Super Special Secret Recipe
+            <ul>
+                <li>1 cup Sugar</li>
+                <li>1 cup Salt</li>
+                <li>1 cup Pepper</li>
+            </ul>
+        </div>
+    );
+};
--- src/index.js
+++ src/index.js
@@ -2,14 +2,23 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 import App from './components/app';
+import Resources from './components/resources';
 import reducers from './reducers';
 
 const createStoreWithMiddleware = applyMiddleware()(createStore);
 
 ReactDOM.render(
   <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
+    <Router>
+      <div>
+        
+      </div>
+        <Switch>
+          <Route path="/" component={App} />
+        </Switch>
+    </Router>
   </Provider>
   , document.querySelector('.container'));
